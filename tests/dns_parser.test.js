c286046f0b577dd49e67821b0e5fe52557867abe
Agustín Formoso <aformoso@ripe.net>
2021-07-01 17:10:37 +0200

    One test case per wireMessage

--- tests/dns_parser.test.js
+++ tests/dns_parser.test.js
@@ -1,13 +1,16 @@
 const parse_wire_message = require('../scripts/dns_parser.js');
 const tests = require('./fixtures.json');
 
-test('Testing parser output', () => {
+// one test case per wireMessage
+Object.entries(tests).map(
+    ([wireMessage, parsedOutput]) => {
+        test(`Testing parser output ${wireMessage.slice(0,8)}`, () => {
 
-    for (let [wireMessage, parsedOutput] of Object.entries(tests)) {
-        expect(
-            parse_wire_message(wireMessage)
-        ).toStrictEqual(  // .toBe() does a shallow comparison and fails...
-            parsedOutput
-        )
+            expect(
+                parse_wire_message(wireMessage)
+            ).toStrictEqual(  // .toBe() does a shallow comparison and fails...
+                parsedOutput
+            )
+        });
     }
-});
+)
